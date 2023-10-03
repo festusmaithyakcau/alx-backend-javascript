943c581dbd609f0ce004a7dca7bb0e5c45254e91
festusmaithyakcau <festusmaithya264@gmail.com>
2023-10-03 06:42:10 -0700

    function getListStudentIds that returns an array of ids from a list of object.
    This function is taking one argument which is an array of objects - and this array is the same format as getListStudents from the previous task.
    
    If the argument is not an array, the function is returning an empty array.
    
    You must use the map function on the array.

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -5,7 +5,7 @@
  *   firstName: String,
  *   location: String
  * }[]} students - The list of students.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Festus Maithya <https://github.com/festusmaithyakcau>
  * @returns
  */
 export default function getListStudentIds(students) {
